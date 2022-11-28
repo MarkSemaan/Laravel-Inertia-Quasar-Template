50197ecff4a14c4d4a9f0f9fe19024dffaa4d906
MarkSemaan <marksemaanwork@gmail.com>
2022-11-28 08:14:17 +0200

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card">
+            <q-card-section>
+              Hello World!
+            </q-card-section>
+          </q-card>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="">
+
+</style>
